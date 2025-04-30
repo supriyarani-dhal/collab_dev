7ac264f90931712c01caa04ecac1d655a356d9ae
supriyarani-dhal <supriyadhal50@gmail.com>
2025-04-30 12:40:56 +0530

    feat: add PWA support with vite-plugin-pwa and update manifest
    - Added vite-plugin-pwa to enhance PWA capabilities.
    - Removed old manifest.json file and replaced it with PWA configuration in vite.config.js.
    - Added new PWA logo image for application branding.
    - Configured caching strategies for documents, scripts, styles, and images.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,60 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import jsconfigPaths from "vite-jsconfig-paths";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), jsconfigPaths()],
+  plugins: [react(), jsconfigPaths(),
+    VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.svg', 'robots.txt'], // Add icons if any
+    manifest: {
+      name: 'coding capsule',
+      short_name: 'coding capsule',
+      description: 'Real-time collaborative code editor with compiler support',
+      theme_color: '#0f172a',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: 'pwa_logo.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa_logo.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: ({ request }) =>
+            request.destination === 'document' ||
+            request.destination === 'script' ||
+            request.destination === 'style',
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'app-cache',
+          }
+        },
+        {
+          urlPattern: ({ request }) => request.destination === 'image',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'image-cache',
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 60 * 60 * 24 * 30 // 30 Days
+            }
+          }
+        }
+      ]
+    }
+  })
+],
 });
